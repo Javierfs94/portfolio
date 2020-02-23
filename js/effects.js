b8627f7c53392a7c3b7afac5d4e4376154c962c5
Javier Frías Serrano <javifs94@gmail.com>
2020-02-23 10:41:29 +0100

    Pendiente arreglar Skills y Menu

---
+++ js/effects.js
@@ -0,0 +1,45 @@
+$(document).ready(init);
+
+let invisible = true;
+
+let ocultar = () => {
+    $('nav').animate({
+        left: '-100%'
+    });
+    invisible = true;
+    mostrarOpen();
+}
+
+let mostrarOpen = () => {
+    $("#close").css('display', 'none');
+    $("#open").css('display', 'inline');
+}
+
+let mostrarClose = () => {
+    $("#close").css('display', 'inline');
+    $("#open").css('display', 'none');
+}
+
+function init() {
+    $('.menu').click(function(event) {
+        event.preventDefault();
+
+        if (invisible == true) {
+            $('nav').animate({
+                left: '0'
+            });
+            mostrarClose();
+            invisible = false;
+        } else {
+            invisible = true;
+            $('nav').animate({
+                left: '-100%'
+            });
+            mostrarOpen();
+        }
+        $('.option').click(ocultar);
+        $('main').click(ocultar);
+    });
+
+    $('nav li').click(mostrarOpen);
+};
